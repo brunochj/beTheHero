5384bd58d3d6f4c6d4a8627913ab348a6b41c89a
brunochj <brunochang95@gmail.com>
2020-03-25 14:08:35 -0300

    front-end da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// // import Logon from './pages/Logon';
+import Routes from './routes';
+import './global.css';
 
 function App() {
+  // const [counter, setCounter] = useState(0);
+
+  // o useState retorna um Array com um valor da variável, e o segundo retorno é uma função de atualização do valor
+
+  // function increment() {
+  //   setCounter(counter+1);
+  //   console.log(counter);
+  // };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Logon></Logon>
+    <Routes></Routes>
   );
 }
 
