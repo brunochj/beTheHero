27041cc24bc536bc1f4d5a2fa814f347eee6280e
brunochj <brunochang95@gmail.com>
2020-03-26 14:21:56 -0300

    mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,25 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+// import { Text, View } from 'react-native';
+import Route from './src/routes';
+
+export default function App() {
+  return (
+    <Route></Route>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#7159c1',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     color: 'white',
+//     fontSize: 20,
+//     fontWeight: 'bold'
+//   }
+// });
